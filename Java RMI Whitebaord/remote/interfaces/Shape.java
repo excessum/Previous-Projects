94a2087103c63dbc61063e48802fdeb307f5c6ba
Curtis Stokes <StokesC3@cardiff.ac.uk>
2015-04-09 18:35:09 +0100

    Revert "Added Java RMI Whiteboard project"
    This reverts commit daa5adde23d49738f1225803c80c5ab913c0282c.

--- Java RMI Whitebaord/remote/interfaces/Shape.java
+++
@@ -1,9 +0,0 @@
-package remote.interfaces;
-import java.rmi.*;
-
-import remote.objects.GraphicalObject;
-
-public interface Shape extends Remote {
-   int getVersion() throws RemoteException;
-   GraphicalObject getAllState() throws RemoteException;
-}
